7f4a362b8944d773f7e1492aeb728102c0e14e6a
Michał Jackowski <michal.jackowski@goodylabs.com>
2018-11-05 17:27:36 +0100

    Updates

---
+++ precache-manifest.a8d058d79628fe66f7728c00bd8fd686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a76cb5eedc9251539370cfa63aa0e1a",
+    "url": "/jsdev-bldr/static/media/hs.0a76cb5e.png"
+  },
+  {
+    "revision": "c1d59989a41ff66b9f37",
+    "url": "/jsdev-bldr/static/js/runtime~main.c1d59989.js"
+  },
+  {
+    "revision": "86668d9807ed418a21e7",
+    "url": "/jsdev-bldr/static/js/main.86668d98.chunk.js"
+  },
+  {
+    "revision": "46becef379ead73ec49f",
+    "url": "/jsdev-bldr/static/js/1.46becef3.chunk.js"
+  },
+  {
+    "revision": "86668d9807ed418a21e7",
+    "url": "/jsdev-bldr/static/css/main.89709dcf.chunk.css"
+  },
+  {
+    "revision": "2047ffaccecec863cb39cadd09c34724",
+    "url": "/jsdev-bldr/index.html"
+  }
+];
